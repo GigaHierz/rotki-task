d37c86dc87520ed0b1189c551a03e7b646ee6774
GigaHierz <lenahierzi@hotmail.de>
2022-12-13 20:32:59 +0100

    ignore linting

--- client/src/stores/BalanceStore.ts
+++ client/src/stores/BalanceStore.ts
@@ -2,7 +2,27 @@ import { defineStore } from 'pinia'
 
 export const useBalanceStore = defineStore('BalanceStore', {
   state: () => ({
-    events: [],
-    event: {}
-  })
+    response: {},
+    accounts: [],
+    balances: []
+  }),
+  getters: {
+    getAccounts () {
+      this.accounts = Object.keys(this.response)
+    },
+    getBalances () {}
+  },
+  actions: {
+    fetchBalances () {
+      return BalanceStore.getBalances()
+        .then(response => {
+          console.log(response)
+
+          this.response = response
+        })
+        .catch(error => {
+          throw error
+        })
+    }
+  }
 })
