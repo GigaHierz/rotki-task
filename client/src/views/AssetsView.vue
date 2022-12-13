9a2b01bf69f1b4996ae4d9627f0d1e2d5f8d0305
GigaHierz <lenahierzi@hotmail.de>
2022-12-13 14:14:38 +0100

    Add Views

---
+++ client/src/views/AssetsView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AssetList from "../components/AssetList.vue";
+import { client } from "../../utils/client";
+
+let userData;
+const fetchData = async () => {
+  const path = await client("balances").then((data) => {
+    console.log(data);
+    userData = data.balances;
+  });
+  console.log(path);
+};
+
+fetchData();
+</script>
+
+<template>
+  <main>
+    <AssetList :balances="userData" />
+  </main>
+</template>
+
+<style lang="scss"></style>
---
+++ client/src/views/EventView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import EventList from "../components/EventList.vue";
+import { client } from "../../utils/client";
+import { TransactionEvents } from "../types/types";
+
+let userData: Array<TransactionEvents>;
+
+const fetchData = async () => {
+  await client("events").then((data) => {
+    console.log(data);
+    userData = data.events?.result;
+  });
+};
+
+fetchData();
+</script>
+
+<template>
+  <EventList :events="userData" />
+</template>
+
+<style lang="scss">
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
